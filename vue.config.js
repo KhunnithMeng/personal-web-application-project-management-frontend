dcb888d79914bea9e3aa8408a02b272e731e974a
Khunnith <khunnithmeng@gmail.com>
2026-03-04 16:42:51 +0700

    Configure to fix resize autocomplete error

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  devServer: {
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false,
+        runtimeErrors: (error) => {
+          if (
+              error?.message?.includes('ResizeObserver loop completed') ||
+              error?.message?.includes('ResizeObserver loop limit exceeded')
+          ) {
+            return false
+          }
+          return true
+        }
+      }
+    }
+  }
 })
