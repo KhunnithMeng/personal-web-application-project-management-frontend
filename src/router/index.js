fddbe57edcc6c540644a83d5557ea4b24c0ef856
Khunnith <khunnithmeng@gmail.com>
2026-03-06 17:03:12 +0700

    Implement route for create task page

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,17 @@ import {createRouter, createWebHistory} from "vue-router";
 import ProjectList from "@/views/project/ProjectList.vue";
 import TaskList from "@/views/task/TaskList.vue";
 import ProjectDetail from "@/views/project/ProjectDetail.vue";
+import TaskDetail from "@/views/task/TaskDetail.vue";
 
 const routes = [
     {path: '/', redirect: '/project'},
+
     {path: '/project',component: ProjectList},
     {path: '/project/add',component: ProjectDetail},
     {path: '/project/edit/:id',component: ProjectDetail},
-    {path: '/task', component: TaskList}
+
+    {path: '/task', component: TaskList},
+    {path: '/task/add', component: TaskDetail}
 ];
 export const router = createRouter({
     routes,
