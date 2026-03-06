233de1eae727e07b3a032e276c71bfdeace4da87
Khunnith <khunnithmeng@gmail.com>
2026-03-06 17:19:21 +0700

    Update status color

--- src/constants/taskStatus.js
+++ src/constants/taskStatus.js
@@ -1,6 +1,6 @@
 export const TASK_STATUSES = Object.freeze([
-    { title: 'To do', value: 'todo', icon: 'mdi-alert' },
-    { title: 'In Progress', value: 'inprogress', icon: 'mdi-progress-clock' },
-    { title: 'Completed', value: 'completed', icon: 'mdi-check-circle' },
-    { title: 'Blocked', value: 'blocked', icon: 'mdi-block-helper' },
+    { title: 'To do', value: 'todo', icon: 'mdi-alert', color: 'yellow' },
+    { title: 'In Progress', value: 'inprogress', icon: 'mdi-progress-clock', color: 'blue' },
+    { title: 'Completed', value: 'completed', icon: 'mdi-check-circle', color: 'green' },
+    { title: 'Blocked', value: 'blocked', icon: 'mdi-block-helper', color: 'red' },
 ])
