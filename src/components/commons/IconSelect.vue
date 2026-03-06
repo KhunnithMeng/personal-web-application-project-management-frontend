00ca1e77d25b4217c97d5e905dceb1f24a879a90
Khunnith <khunnithmeng@gmail.com>
2026-03-06 16:57:45 +0700

    Implement reuse icon selection component

---
+++ src/components/commons/IconSelect.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+defineProps({
+  items: {
+    type: Array,
+    required: true
+  },
+  label: String,
+  modelValue: [String, Number]
+});
+
+const emit = defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <v-select label="Status"
+            variant="underlined"
+            :items="items"
+            item-title="title"
+            item-value="value"
+            :model-value="modelValue"
+            @update:model-value="emit('update:modelValue', $event)">
+    <template #selection="{ item }">
+                  <span>
+                    <v-icon :icon="item.raw.icon"
+                            size="18"
+                            class="mr-2"></v-icon> {{ item.title }}
+                  </span>
+    </template>
+
+    <template #item="{ props, item }">
+      <v-list-item v-bind="props">
+        <template #prepend>
+          <v-icon :icon="item.raw.icon"></v-icon>
+        </template>
+      </v-list-item>
+    </template>
+  </v-select>
+</template>
+
+<style scoped>
+
+</style>
