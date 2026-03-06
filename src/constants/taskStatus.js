2db818510e2e7ef6504e9478f1055826d392b1fa
Khunnith <khunnithmeng@gmail.com>
2026-03-06 17:02:44 +0700

    Create constant task status data

---
+++ src/constants/taskStatus.js
@@ -0,0 +1,6 @@
+export const TASK_STATUSES = Object.freeze([
+    { title: 'To do', value: 'todo', icon: 'mdi-alert' },
+    { title: 'In Progress', value: 'inprogress', icon: 'mdi-progress-clock' },
+    { title: 'Completed', value: 'completed', icon: 'mdi-check-circle' },
+    { title: 'Blocked', value: 'blocked', icon: 'mdi-block-helper' },
+])
