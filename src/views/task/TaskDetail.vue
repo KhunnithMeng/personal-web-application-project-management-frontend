16b57cb6617174a9bda10f1136ce5e8df507dc76
Khunnith <khunnithmeng@gmail.com>
2026-03-09 13:48:05 +0700

    Create task detail form feature both ui and integration

--- src/views/task/TaskDetail.vue
+++ src/views/task/TaskDetail.vue
@@ -1,15 +1,55 @@
 <script setup>
 import {router} from "@/router";
 import IconSelect from "@/components/commons/IconSelect.vue";
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
 import {TASK_STATUSES} from "@/constants/taskStatus";
+import {TASK_PRIORITY} from "@/constants/taskPriority";
+import {getTags} from "@/services/tag-service";
+import {getProjects} from "@/services/project-service";
+import {useLoader} from "@/composibles/useLoader";
+import {createTask} from "@/services/task-service";
+import {useMessage} from "@/composibles/useMessage";
 
-const taskForm = ref({
-  status: 'todo'
+const { loader } = useLoader();
+const { showMessage } = useMessage();
+
+const tags = ref([]);
+const projects = ref([]);
+const form = ref(null);
+const task = ref({
+  title: '',
+  description: '',
+  status: 'todo',
+  priority: 'low',
+  estimatedHours: 0,
+  actualHours: 0,
+  deadline: null,
+  tagIds: [],
+  projectId: null
+});
+
+onMounted(() => {
+  getTags().then(res => tags.value = res || []);
+  getProjects().then(res => projects.value = res || [])
 })
-function submit() {
-  console.log(taskForm.value)
+
+async function submit() {
+  const { valid } = await form.value.validate();
+  if (!valid) {
+    showMessage('Please check the highlighted fields — some information is missing or incorrect.', 'error');
+    return;
+  }
+
+  loader.value = true;
+  createTask(task.value.projectId, task.value)
+      .then(() => {
+        showMessage('Task is successfully created');
+        router.back();
+      })
+      .catch(res => showMessage(res.message, 'error'))
+      .finally(() => loader.value = false);
 }
+
 </script>
 
 <template>
@@ -33,28 +73,83 @@ function submit() {
   </div>
 
   <v-card class="mx-auto">
-    <v-form>
+    <v-form ref="form">
       <v-container>
         <v-row class="align-start">
           <v-col>
+            <v-select label="Project"
+                      variant="underlined"
+                      required
+                      autocomplete="off"
+                      item-title="name"
+                      item-value="id"
+                      v-model="task.projectId"
+                      :rules="[v => !!v || 'Project is required']"
+                      :items="projects"></v-select>
+
             <v-text-field placeholder="Title"
                           variant="underlined"
                           clearable
+                          required
                           label="Title"
-                          name="title"></v-text-field>
+                          name="title"
+                          v-model="task.title"
+                          :rules="[v => !!v || 'Title is required']"></v-text-field>
 
             <v-textarea label="Description (Optional)"
                         clearable
+                        rows="7"
+                        v-model="task.description"
                         counter></v-textarea>
           </v-col>
           <v-divider vertical></v-divider>
           <v-col>
             <v-row class="align-start">
               <v-col>
-                <IconSelect :items="TASK_STATUSES" label="Status" v-model="taskForm.status"></IconSelect>
+                <IconSelect :items="TASK_STATUSES" label="Status" v-model="task.status"></IconSelect>
               </v-col>
               <v-col>
-                <v-select label="Priority" variant="underlined" :items="['in progress', 'active']"></v-select>
+                <IconSelect :items="TASK_PRIORITY" label="Priority" v-model="task.priority"></IconSelect>
+              </v-col>
+            </v-row>
+            <v-row >
+              <v-col>
+                <v-number-input label="Estimated Hours"
+                                variant="underlined"
+                                :min="0"
+                                v-model="task.estimatedHours"></v-number-input>
+              </v-col>
+              <v-col>
+                <v-number-input label="Actual Hours"
+                                variant="underlined"
+                                :min="0"
+                                v-model="task.actualHours"></v-number-input>
+              </v-col>
+            </v-row>
+            <v-row >
+              <v-col>
+                <v-date-input prepend-icon=""
+                              prepend-inner-icon="$calendar"
+                              label="Dateline"
+                              v-model="task.deadline"
+                              persistent-placeholder
+                              variant="underlined"
+                              autocomplete="off"></v-date-input>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <v-combobox variant="underlined"
+                            chips
+                            clearable
+                            item-value="id"
+                            item-title="name"
+                            v-model="task.tagIds"
+                            autocomplete="off"
+                            multiple
+                            :return-object="false"
+                            :items="tags"
+                            placeholder="Tags"></v-combobox>
               </v-col>
             </v-row>
           </v-col>
