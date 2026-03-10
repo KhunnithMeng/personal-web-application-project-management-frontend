6c7fff1cca99b203fc7d479001ee97ce9ac08064
Khunnith <khunnithmeng@gmail.com>
2026-03-10 15:04:48 +0700

    Create date locale to format accurate date feature

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,3 +1,12 @@
 export function formatDate(date) {
     return date ? new Date(date).toDateString() : null;
 }
+
+export function formatDateLocal(date) {
+   const d = new Date(date);
+   const year = d.getFullYear();
+   const month = String(d.getMonth() + 1).padStart(2, "0");
+   const day = String(d.getDate()).padStart(2, "0");
+
+   return `${year}-${month}-${day}`;
+}
